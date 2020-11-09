cdedc541feabab3fd47aaae549d0d3d1f9d7734d
nicoWeb31 <nico.riot@free.fr>
2020-11-09 20:09:41 +0100

    multiple promise

--- 3-asynchronous-JS/starter/index.js
+++ 3-asynchronous-JS/starter/index.js
@@ -20,21 +20,49 @@ const writeFilePromise = (file, data) => {
     });
 };
 
-//consume promise
-readFilePromise(`${__dirname}/dog.txt`)
-    .then((data) => {
+const getDogPic = async () => {
+    try {
+        const data = await readFilePromise(`${__dirname}/dog.txt`);
         console.log(`Breed: ${data}`);
-        return superagent.get(
+
+        const res = await superagent.get(
             `https://dog.ceo/api/breed/${data}/images/random`
         );
-    })
-    .then((response) => {
-        console.log(response.body);
-        return writeFilePromise('dog-img.txt', response.body.message);
-    })
-    .then(() => {
+        console.log(res.body);
+
+        await writeFilePromise('dog-img.txt', res.body.message);
         console.log(`Rondom dog image savedto file !`);
-    })
-    .catch((err) => {
-        console.log(err.message);
-    });
+    } catch (err) {
+        console.log('err', err);
+    }
+    return console.log('2 : ok')
+};
+(async () => {
+    try {
+        console.log('1 : will get dog pics!' )
+        const x = await getDogPic();
+        console.log(x)
+        console.log('3 : Done getting dog!' )
+    } catch (err) {
+        console.log('err:  💣 ')
+    }
+})();
+
+//consume promise
+// readFilePromise(`${__dirname}/dog.txt`)
+//     .then((data) => {
+//         console.log(`Breed: ${data}`);
+//         return superagent.get(
+//             `https://dog.ceo/api/breed/${data}/images/random`
+//         );
+//     })
+//     .then((response) => {
+//         console.log(response.body);
+//         return writeFilePromise('dog-img.txt', response.body.message);
+//     })
+//     .then(() => {
+//         console.log(`Rondom dog image savedto file !`);
+//     })
+//     .catch((err) => {
+//         console.log(err.message);
+//     });
